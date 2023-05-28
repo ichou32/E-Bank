e7d6beb80ffd97b27d4e618ee49966a1c6b664f4
ichou <ichou.bahba@gmail.com>
2023-05-28 10:14:01 +0100

    init repo

---
+++ src/main/java/enset/bdcc/digitalbanque/DigitalBanqueApplication.java
@@ -0,0 +1,13 @@
+package enset.bdcc.digitalbanque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalBanqueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalBanqueApplication.class, args);
+    }
+
+}
